2de4318d324937f326aff66becaf562de2a4486a
DESKTOP-EJ74F6G\matts <matthew.stobbs@echobox.com>
2022-02-21 15:52:46 +0000

    Install prop-types

--- 10.jsx
+++ 10.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Button({ children, isLoading, loadingText, size, onClick }) {
   const handleClick = () => {
     onClick();
-  }
+  };
 
   return (
     <button
